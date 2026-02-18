48bd9fdcb970ebf5d565900e8c6901c8572cbe3e
DanielXie0348 <ds3xie@uwaterloo.ca>
2026-02-17 22:12:43 -0500

    Fix

--- modules/detect_colours.py
+++ modules/detect_colours.py
@@ -56,8 +56,7 @@ class DetectBlue:
         # Apply the threshold for the colour detection
         mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
-        # Shows the detected colour from the mask
-        res = cv2.bitwise_and(img, img, mask=mask)
+        
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
